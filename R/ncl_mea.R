d0f54d866abf22f91557b84a0c9709d6807dab47
sje30 <empty>
2007-01-15 15:37:54 +0000

    Initial revision

---
+++ R/ncl_mea.R
@@ -0,0 +1,216 @@
+mcd.data.to.array <- function(file, beg=NULL, end=NULL) {
+
+  ## Read in the MCD data file.  Return the spike trains and the channel
+  ## names that are used.
+  ## Spikes outside of the time window [BEG, END] (if non-null) are ignored.
+  ## BEG and END are given in seconds.
+  ## Recall that blank lines will not be read into the data matrix.
+  
+  data <- read.table(file, as.is=T, skip=2, fill=T)
+  
+
+  ## Row number of the start of each spike train.
+  channel.start <- which(data[,2] =='Spikes')
+  n.channels <- length(channel.start)
+  
+  ## For the last channel, we add where the end of the file is.
+  ## This is so that our check for no spikes will also work for the last
+  ## channel in the data set.
+  channel.start <- c(channel.start, nrow(data))
+
+  ## Name of each channel.
+  channels <- data[channel.start,4]
+  channel.ok <- rep(0, n.channels)
+  spikes <- list(); n <- 0
+  
+  for (channel  in 1:n.channels) {
+    beg.spike <- channel.start[channel]+2 #first spike for this channel
+    if (beg.spike < channel.start[channel+1]) {
+      ## We have some data.
+      end.spike <- channel.start[channel+1] - 1 #last line
+
+      ## convert data from msec to seconds.
+      this.train <- as.numeric(data[beg.spike:end.spike,1]) / 1000
+
+      if (!is.null(beg)) {
+        rejs <- which(this.train < beg)
+        if (any(rejs))
+          this.train <- this.train[-rejs]
+      }
+
+      if ( any(this.train) && !is.null(end)) {
+        rejs <- which(this.train > end)
+        if (any(rejs))
+          this.train <- this.train[-rejs]
+      }
+
+      if (any(this.train)) {
+        n <- n + 1
+        spikes[[n]] <- this.train
+        channel.ok[n] <- channels[channel]
+      }
+
+    }
+
+  }
+
+  channel.ok <- channel.ok[1:n]         #truncate to right length.
+  res <- list(spikes=spikes, channels=channel.ok) 
+}
+
+
+
+
+
+ncl.read.spikes <- function(filename, scale=200, ids=NULL,
+                            time.interval=1, beg=NULL, end=NULL) {
+
+  ## Read in Ncl data set.  Scale gives the distance in um between
+  ## adjacent channels.  This is 200um by default.  This can be
+  ## changed to cope with the developmental changes in retina.  IDS is
+  ## an optional vector of cell numbers that should be analysed -- the
+  ## other channels are read in but then ignored.
+
+
+  ## Tue 25 Jul 2006 -- I'm not sure if this is the cleanest way to read in the
+  ## data files, as it probably can be read in using just
+  ## read.csv(filename, sep='\t')
+  ## but since this works, fine.
+  
+
+  dat <- mcd.data.to.array(filename, beg, end)
+  spikes <- dat$spikes
+  channels <- dat$channels
+
+  spikes.range <- range(unlist(spikes))
+  if (is.null(beg))  beg <-  spikes.range[1]
+  if (is.null(end))  end <-  spikes.range[2]
+  rec.time <- c(beg, end)
+
+  
+  ## Count the number of spikes per channel, and label them.
+  nspikes <- sapply(spikes, length)
+
+  ## TODO
+  ## names(nspikes) <- channels 
+
+  ## meanfiring rate is the number of spikes divided by the (time of
+  ## last spike - time of first spike).  
+  ##meanfiringrate <- nspikes/ ( sapply(spikes, max) - sapply(spikes, min))
+  ## todo -- find min, max time.
+
+  meanfiringrate <- nspikes/ ( end - beg)
+
+  ## correct the above in the sanger data set; it should not be
+  ## guessing at the time of the recording.
+  
+  ## todo: spike names need to be found.
+  
+  ## Parse the channel names to get the cell positions.
+  ## Note that we currently ignore any label that comes after the digits
+  ## for the channel.
+  ## e.g. when more than one cell is assigned to the same channel, we
+  ## can have "ch_13a" and "ch_13b".  If there is only one cell on a channel
+  ## that channel is written "ch_13".
+  ## In Jay's prog, rows are numbered from the top, downwards.  In R, we
+  ## have the reverse.  To align them in R, we would need to subtract 9 from
+  ## rows.
+  
+  ##cols <- as.integer(substring(channels, 4,4)) * scale
+  ##rows <- as.integer(substring(channels, 5,5)) * scale
+  cols <- as.integer(substring(channels, 1,1)) * scale
+  rows <- as.integer(substring(channels, 2,2)) * scale
+  pos <- cbind(cols, rows)
+  class(pos) <- "jay.pos"
+
+  ## temporary test: shuffle electrode positions.
+  ## pos <- pos[sample(1:num.channels),]
+  
+  ## check that the spikes are monotonic.
+  check.spikes.monotonic(spikes)
+
+  dists <- make.distances(pos)
+
+  ## Test code:
+  ## corr <- matrix(data=c(0, 0, 0, 4, 0, 0, 5, 7, 0),nrow=3)
+  ## dists <- matrix(data=c(0,0,0, 1, 0,0, 9,8,0),nrow=3)
+  ## cbind( my.upper(dists), my.upper(corr))
+
+  ## Electrodes are spaced 100um apart in Jay's rectangular array.
+  ## Sanger- add 2000 bin!
+  jay.distance.breaks <- c(0, 150, 250, 350, 450, 550, 650, 1000, 2000)
+  jay.distance.breaks.strings <-
+    levels(cut(0, jay.distance.breaks, right=FALSE, include.lowest=TRUE))
+
+  dists.bins   <- bin.distances(dists, jay.distance.breaks)
+
+  ## Mon 25 Nov 2002: explore longer timescales?
+  corr.indexes.dt <- 0.05               #time window for coincident spikes
+  ##corr.indexes.dt <- 10.00 # try longer, such as 1s, 5s or 10s.
+  if (length(spikes) > 1) {
+    corr.indexes <- make.corr.indexes(spikes, corr.indexes.dt)
+    corr.id <- cbind(my.upper(dists), my.upper(corr.indexes))
+    corr.id.means <- corr.get.means(corr.id)
+  } else {
+    corr.indexes <- NA
+    corr.id <- NA
+    corr.id.means <- NA
+  }
+
+  rates <- make.spikes.to.frate(spikes, time.interval=time.interval)
+  
+  ## See if we need to shift any units.  this affects only the
+  ## visualisation of the units in the movies.  We assume that "shifted"
+  ## positions are stored in the file with same name as data file
+  ## except that the .txt is replaced with .sps.  Then each line of this
+  ## file contains three numbers:
+  ## c dx dy
+  ## where c is the cell number to move, and dx,dy is the amount (in um)
+  ## by which to move the cells.  If you edit the file, this function
+  ## must be called again for the new values to be read in.
+  ## The shifted positions are used only by the movie functions and
+  ## by the function plot.shifted.jay.pos(s) [this shows all units].
+
+
+  ## Tue 19 Dec 2006: this assumes filename ends in .txt; do not worry
+  ## about this for now.
+  
+  shift.filename <- sub("\\.txt$", ".sps", filename)
+  unit.offsets <- NULL                  #default value.
+  if (FALSE && file.exists(shift.filename)) { #TODO -- why running?
+    updates <- scan(shift.filename)
+    ## must be 3 data points per line
+    stopifnot(length(updates)%%3 == 0)
+    updates <- matrix(updates, ncol=3, byrow=TRUE)
+    units <- updates[,1]
+    if (any(units> length(spikes))) {
+      stop(paste("some units not in recording...",
+                 paste(units[units>=length(spikes)],collapse=",")))
+    }
+    unit.offsets <- pos*0               #initialise all elements to zero.
+    unit.offsets[units,] <- updates[,2:3]
+  }
+  
+  
+  
+  res <- list( channels=channels,
+              spikes=spikes, nspikes=nspikes, NCells=length(spikes),
+              meanfiringrate=meanfiringrate,
+              file=filename,
+              pos=pos,
+              scale=scale,
+              dists=dists, dists.bins=dists.bins,
+              corr.indexes=corr.indexes,
+              corr.indexes.dt=corr.indexes.dt,
+              corr.id=corr.id,
+              corr.id.means=corr.id.means,
+              distance.breaks=jay.distance.breaks,
+              distance.breaks.strings=jay.distance.breaks.strings,
+              rates=rates,
+              unit.offsets=unit.offsets,
+              rec.time=rec.time
+              )
+  class(res) <- "mm.s"
+  res
+
+}
