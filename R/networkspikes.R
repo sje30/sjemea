7f24cd23d154733b64ef48a52f5290e0745b718f
Stephen Eglen <sje30@cam.ac.uk>
2007-02-13 21:31:51 +0000

    Change order of plotting -- seemed to fix bug!

--- R/networkspikes.R
+++ R/networkspikes.R
@@ -136,7 +136,7 @@ find.peaks <- function(trace) {
 }
 
 
-show.ns <- function(p, nrow=8, ncol=8, ask=FALSE) {
+show.ns <- function(p, nrow=8, ncol=8, ask=FALSE, plot=TRUE) {
   ## Show the network spikes.
 
   ## This code does not check to worry if there is a spike right at either
@@ -146,27 +146,35 @@ show.ns <- function(p, nrow=8, ncol=8, ask=FALSE) {
     cat("*** No network spikes found\n")
     return (NULL)
   }
-
-  old.par <- par(mfrow=c(nrow,ncol), mar=c(1,1,1,1),ask=ask)
-  sur = 40                              #100 normally
+  if (plot) {
+    old.par <- par(mfrow=c(nrow,ncol), mar=c(2.5,1,1,1),ask=ask)
+  }
+  
+  sur = 100                              #100 normally
   ave = rep(0, (2*sur)+1)
   npts = length(counts$sum)
   measures = matrix(NA, nrow=nrow(p), ncol=3)
   
   n.ns = 0                              #Number of valid network spikes found
-  ##for (i in 1:nrow(p)) {
-  for (i in 1:2) {
+  for (i in 1:nrow(p)) {
     peak.i = p[i,1]; lo = (peak.i-sur); hi = peak.i+sur
 
     ## Check that enough data can be found:
     if ( (lo >0) && ( hi < npts) ) {
       n.ns = n.ns + 1
 
-
       measures[n.ns, 3] = peak.i
       dat = counts$sum[lo:hi]
-      abline(v=sur+1)
-      hm = find.halfmax(dat, peak.n=sur+1, frac=0.5)
+      if (plot) {
+        plot(dat, xaxt='n', yaxt='n', ylim=c(0,60),
+             bty='n', type='l',xlab='', ylab='')
+        ##abline(v=sur+1)
+        axis(1, at=c(0,1,2)*sur,
+             labels=c('-300 ms', '0 ms', '+300 ms'))
+      }
+      
+
+      hm = find.halfmax(dat, peak.n=sur+1, frac=0.5, plot=plot)
       measures[n.ns, 2] = hm$durn
 
       dat2 = dat;
@@ -176,8 +184,6 @@ show.ns <- function(p, nrow=8, ncol=8, ask=FALSE) {
       k = kurtosis(dat2)
       measures[n.ns, 1] = k
       ave = ave + dat
-      plot(dat, xaxt='n', yaxt='n', ylim=c(0,60),
-           bty='n', type='l',xlab='', ylab='')
       legend("topleft", paste(round(max(dat))), bty='n')
       ##title(paste('k ', signif(k,3)))
     }
@@ -186,20 +192,27 @@ show.ns <- function(p, nrow=8, ncol=8, ask=FALSE) {
   ## now show the average
   if (n.ns > 0) {
     ave = ave/n.ns
-    plot(ave, xaxt='n', yaxt='n', bty='n', type='l',xlab='', ylab='')
-    legend("topleft", paste("m", round(max(ave))), bty='n')
-    find.halfmax(ave)
+    if (plot) {
+      plot(ave, xaxt='n', yaxt='n', bty='n', type='l',xlab='', ylab='')
+      legend("topleft", paste("m", round(max(ave))), bty='n')
+      find.halfmax(ave)
+    }
+
+    if (plot) {
+      par(old.par)
+    }
 
     ##stripchart(measures[,1], ylab='K', method='jitter', vert=T, pch=19,
     ##main=paste('kurtosis', round(mean(measures[,1]),3)))
-
-    stripchart(measures[,2], ylab='durn (bins)', method='jitter',
-               vert=TRUE, pch=19,
-               main=paste('FWHM durn', round(mean(measures[,2]),3)))
+    if (plot) {
+      stripchart(measures[,2], ylab='durn (bins)', method='jitter',
+                 vert=TRUE, pch=19,
+                 main=paste('FWHM durn', round(mean(measures[,2]),3)))
+    }
   }
-  par(old.par)
 
-  list(measures, ave=ave)
+
+  list(measures, ns.mean=ave)
 }
 
 find.halfmax.cute <- function(y) {
@@ -283,7 +296,7 @@ find.halfmax <- function(y, peak.n=NULL, plot=TRUE, frac=0.5) {
 
   
   if(plot) {
-    abline(v=xl.half, col='green'); abline(v=xr.half, col='green'); #temp
+    ##abline(v=xl.half, col='green'); abline(v=xr.half, col='green'); #temp
     abline(h=peak.val * frac, col='red')
     segments(xl.half, half.max, xr.half, half.max, col='blue')
   }
