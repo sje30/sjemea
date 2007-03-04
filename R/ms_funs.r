bf516be158e4d80265da417bfa8f2c322a0e7085
Stephen Eglen <sje30@cam.ac.uk>
2007-03-04 18:23:20 +0000

    (jay.read.spikes): Update to use new correlation index method.

--- R/ms_funs.r
+++ R/ms_funs.r
@@ -123,41 +123,7 @@ plotCI <- function (x, y = NULL, uiw, liw = uiw,
 
 
 
-## todo -- are the cell positions inverted?
 
-
-plot.corr.index <- function(s, identify=FALSE,
-                            main=paste(basename(s$file), "dt:", s$corr.indexes.dt),
-                            ...) {
-  ## Plot the correlation indices as a function of distance.
-  ## If identify is TRUE, we can locate cell pairs on the plot using
-  ## left mouse button.  
-  dists <- s$dists[which(upper.tri(s$dists))]
-  corrs <- s$corr.indexes[which(upper.tri(s$corr.indexes))]
-
-  xlabel <- expression(paste("distance (", mu, "m)"))
-
-  plot.default(dists, corrs, xlab=xlabel,
-               ylab="correlation index", bty="n",
-               main=main,
-               ...)
-
-  if (identify) {
-    labels1 <- outer(seq(1, s$NCells), seq(1,s$NCells), FUN="paste")
-    labs <- labels1[which(upper.tri(labels1))]
-    identify(dists, corrs, labels=labs)
-  }
-                  
-}
-
-plot.corr.index.fit <- function(s, ...) {
-  ### Show the correlation indexes and then the fit.
-  plot.corr.index(s, identify=FALSE,col="red", log="")
-  plotCI(s$corr.id.means[,1], s$corr.id.means[,2], s$corr.id.means[,3],
-         xlab="distance", ylab="correlation index", 
-         pch=19, add=TRUE)
-  corr.do.fit(s$corr.id,plot=TRUE)
-}
 
 plot.mm.s <- function(s, whichcells=NULL,
                       beg=min(unlist(s$spikes), na.rm=TRUE),
@@ -460,33 +426,6 @@ jay.read.spikes <- function(filename, ids=NULL,
   ## check that the spikes are monotonic.
   check.spikes.monotonic(spikes)
 
-  dists <- make.distances(layout$pos)
-
-  ## Test code:
-  ## corr <- matrix(data=c(0, 0, 0, 4, 0, 0, 5, 7, 0),nrow=3)
-  ## dists <- matrix(data=c(0,0,0, 1, 0,0, 9,8,0),nrow=3)
-  ## cbind( my.upper(dists), my.upper(corr))
-
-  ## Electrodes are spaced 100um apart in Jay's rectangular array.
-  jay.distance.breaks <- c(0, 150, 250, 350, 450, 550, 650, 1000)
-  jay.distance.breaks.strings <-
-    levels(cut(0, jay.distance.breaks, right=FALSE, include.lowest=TRUE))
-
-  dists.bins   <- bin.distances(dists, jay.distance.breaks)
-
-  ## Mon 25 Nov 2002: explore longer timescales?
-  corr.indexes.dt <- 0.05               #time window for coincident spikes
-  ##corr.indexes.dt <- 10.00 # try longer, such as 1s, 5s or 10s.
-  if (length(spikes) > 1) {
-    corr.indexes <- make.corr.indexes(spikes, corr.indexes.dt)
-    corr.id <- cbind(my.upper(dists), my.upper(corr.indexes))
-    corr.id.means <- corr.get.means(corr.id)
-  } else {
-    corr.indexes <- NA
-    corr.id <- NA
-    corr.id.means <- NA
-  }
-
   rates <- make.spikes.to.frate(spikes, time.interval=time.interval,
                                 beg=beg, end=end)
   
@@ -524,19 +463,18 @@ jay.read.spikes <- function(filename, ids=NULL,
               meanfiringrate=meanfiringrate,
               file=filename,
               layout=layout,
-              dists=dists, dists.bins=dists.bins,
-              corr.indexes=corr.indexes,
-              corr.indexes.dt=corr.indexes.dt,
-              corr.id=corr.id,
-              corr.id.means=corr.id.means,
-              distance.breaks=jay.distance.breaks,
-              distance.breaks.strings=jay.distance.breaks.strings,
               rates=rates,
               rec.time=rec.time,
               unit.offsets=unit.offsets
               )
   class(res) <- "mm.s"
+
+  ## Electrodes are spaced 100um apart in Jay's rectangular array.
+  jay.distance.breaks = c(0, 150, 250, 350, 450, 550, 650, 1000)
+  res$corr = corr.index(res, jay.distance.breaks)
+
   res
+
 
 }
 
@@ -587,40 +525,6 @@ shuffle.spike.times <- function (s, noise.sd) {
   s2
 }
 
-bin.distances <- function(dists, breaks) {
-  ## DISTS is a upper NxN array.
-  ## breaks is a vector of breakpoints.
-  ## Return an array of the same size where each distance value is
-  ## given a corresponding bin number.
-
-  ## e.g.
-  ## dists <- matrix( c(0,0,0, 400,0,0, 80, 1000, 0), nrow=3)
-  ## jay.bin.distances(dists)
-  ## This binning procedure can then be checked by comparing the
-  ## distances and their bins:
-  ## plot(my.upper(s$dists.bins), my.upper(s$dists))
-  ## boxplot(my.upper(s$dists)~ my.upper(s$dists.bins))
-  
-  distances <- my.upper(dists)
-  ## These breaks are hardcoded.
-
-  ##data <- c(0, 100, 700, 900, 400)
-
-  ## Make each bin [low, high) with exception that highest bin is
-  ## [low,high]. Labels is false so that we just return numeric count
-  ## of bin, rather than a factor.
-  bins <- cut(distances, breaks, right=FALSE,
-              include.lowest=TRUE, labels=FALSE)
-  invalid <- is.na(bins)
-  if (any(invalid))
-    stop(paste("distances not binned:",
-                  paste(distances[which(invalid)],collapse=" ")))
-  n <- dim(dists)[1]
-  res <- matrix(0, nrow=n, ncol=n)
-  res[which(upper.tri(res))] <- bins
-  
-  res
-}
 
 fourplot <- function(s) {
   ## Simple 2x2 summary plot of an "s" structure.
@@ -631,13 +535,9 @@ fourplot <- function(s) {
   plot(s$layout)                             #show layout of electrodes.
   plot.meanfiringrate(s)
   plot(s)                                 #plot the spikes.
-  if(!is.na(s$corr.indexes)) {
-    ## Plot correlation indexes only if we have them.
-    plot.corr.index(s, identify=F,col="red", log="")
-    plotCI(s$corr.id.means[,1], s$corr.id.means[,2], s$corr.id.means[,3],
-           xlab="distance", ylab="correlation index", 
-           pch=19, add=TRUE)
-    corr.do.fit(s$corr.id,plot=TRUE)
+
+  if(!is.na(s$corr$corr.indexes[1])) {
+    plot.corr.index(s)
   }
 }
 
@@ -661,68 +561,6 @@ make.distances <- function(posns) {
 
   dists
 }
-
-make.corr.indexes <- function(spikes, dt) {
-  ## Return the correlation index values for each pair of spikes.
-  ## The matrix returned is upper triangular.
-  ## SPIKES should be a list of length N, N is the number of cells.
-  ## "dt" is the maximum time for seeing whether two spikes are coincident.
-  ## This is defined in the 1991 Meister paper.
-  n <- length(spikes)
-  if (n == 1) {
-    ## If only one spike train, cannot compute the cross-corr indexes.
-    0;
-  } else {
-    Tmax <- max(unlist(spikes))           #time of last spike.
-    Tmin <- min(unlist(spikes))           #time of first spike.
-    corrs <- array(0, dim=c(n,n))
-    for ( a in 1:(n-1)) {
-      n1 <- length(spikes[[a]])
-      for (b in (a+1):n) {
-        n2 <- length(spikes[[b]])
-        corrs[a,b] <-  as.double(count.nab(spikes[[a]], spikes[[b]],dt) *
-                                 (Tmax-Tmin)) /
-                                   (as.double(n1) * n2 * (2*dt))
-      }
-    }
-    if (any(is.na(corrs))) {
-      stop("corrs has some NA values -- possible integer overflow in n1*n2, or zero spikes in one of the trains?")
-    }
-    corrs
-  }
-}
-
-
-
-corr.index.means <- function(x) {
-  ## Compute the mean,sd correlation index at each given distance.
-  dists <- x$dists[which(upper.tri(x$dists))]
-  corrs <- x$corr.indexes[which(upper.tri(x$corr.indexes))]
-
-  dists.uniq <- unique(dists)
-  num.dists <- length(dists.uniq)       #num of  different distances.
-
-  ##print(dists.uniq)
-  ## create 4-D array to store results.  Each row stores the
-  ## distance, mean corr, sd, and num of values at that distance.
-
-  res <- array(0,  dim=c(num.dists,4))
-  colnames(res) <- c("dist","mean corr", "sd", "n")
-  
-  i <- 1
-
-  for (d in dists.uniq) {
-    ## find all correlations for pairs within 0.01um of given distance.
-    cs <- corrs[ which(abs(dists-d)<0.01)]
-    corrs.mean <- mean(cs)
-    corrs.sd   <- sd(cs)
-    res[i,] <- c(d, corrs.mean, corrs.sd, length(cs))
-    i <- 1+i
-  }
-
-  res
-}
-
 
 ######################################################################
 ## Mutual information code.
@@ -827,79 +665,6 @@ prob.t <- function(p.t.cond.r, p.r)  {
   p.t
 }
 
-corr.get.means <- function(id) {
-  ## Compute the mean,sd of the correlation index at each distance.
-  ## id is the array of [n,2] values.  Each row is [d,i].
-  ## Returns a matrix.
-  
-  corr.get.means.helper <- function(x) {
-    ## Helper function to create  mean and sd of one set of distances.
-    indexes <- which(id[,1] == x)
-    c(x, mean(id[indexes,2]), sd(id[indexes,2]), length(indexes))
-    ##c(x, median(id[indexes,2]), mad(id[indexes,2]), length(indexes))
-  }
-  
-  d.uniq <- sort(unique(id[,1]))
-  means <- t(sapply(d.uniq, corr.get.means.helper))
-  colnames(means) <- c("dist", "mean", "sd", "n")
-  means
-}
-
-corr.do.fit <- function(id, plot=TRUE, show.ci=FALSE, ...) {
-  ## Do the fit to the exponential and optionally plot it.  Any
-  ## correlation index of zero is removed, since we cannot take the
-  ## log of zero.  Hopefully there won't be too many of these.
-  ## If SHOW.CI is true, do the fit with 95% confidence intervals.
-
-  y.zero <- which(id[,2]==0)
-  if (length(y.zero)>0) {
-    id <- id[-y.zero,]
-    warning(paste("removing", length(y.zero),"zero entries"))
-  }
-  x <- id[,1]
-  y.log <- log(id[,2])
-  fit <- lm(y.log ~ x)
-  if (show.ci) {
-    expt.new <- data.frame(x = seq(0, 850, 10))  #range of x for predictions.
-    expt.clim <- predict(fit, expt.new, interval="confidence")
-  }
-  if (plot)  {
-    if (show.ci) {
-      ## Confidence intervals will show mean, so don't need
-      ## to do both matlines and curve.
-      matlines(expt.new$x, exp(expt.clim), lty=c(1,2,2),
-               col="black")
-    } else {
-      curve(exp(fit$coeff[1]) * exp(x * fit$coeff[2]), add = TRUE,
-            from=0, ...)
-    }
-  }
-  fit
-}
-
-corr.check.fit <- function() {
-  ## Simple test routine to see that the exponential fits are okay.
-  a <- 40; b <- .01
-  x <- seq(from=1, to=500, by=20)
-  y <- a*exp(-b*x) + (2*rnorm(length(x)))
-  plot(x,y, log="y")
-  fit <- corr.do.fit( cbind(x,y), col=p9.col)
-  
-  ## should be similar to (a,b)
-  print(exp(fit$coefficients))
-}
-
-my.upper <- function (x,diag=FALSE) {
-  ## Return the upper triangular elements of a matrix on a
-  ## column-by-column basis.
-  ## e.g. my.upper(matrix(1:9, nrow=3), diag=TRUE)
-  ## returns >>1 4 5 7 8 9<<
-  if (is.matrix(x)) {
-   x[ which(upper.tri(x,diag))]
-  } else {
-    stop(paste(deparse(substitute(x)),"is not a matrix"))
-  }
-}
 
 
 make.mi <- function(s, tmax=4) {
