e813eaea0014324178452e0da898f70605345a55
Stephen Eglen <sje30@cam.ac.uk>
2009-09-21 12:24:22 +0000

    Initial revision

---
+++ R/iit.R
@@ -0,0 +1,127 @@
+## IIT.R code
+
+iit.read.spikes <- function(filename, ids=NULL,
+                              time.interval=1, beg=NULL, end=NULL) {
+  ## Read in data from IIT.
+  ## FILENAME: spike times, stored in matlab sparse arrays.
+  ## IDS: an optional vector of cell numbers that should be analysed
+  ## -- the other channels are read in but then ignored.
+
+  if (!require(R.matlab))
+    stop('The R.matlab package is needed for this routine.  Please install.')
+  
+  z <- readMat(filename)
+  frame.rates = 7800
+  spikes <- lapply(z, function(e) {e@i/frame.rates})
+
+  ## Now remove spikes outside of range required.
+  spikes.range <- range(unlist(spikes))
+  if (!is.null(end)) {
+    spikes <- lapply(spikes, jay.filter.for.max, max=end)
+  } else {
+    end <- spikes.range[2]
+  }
+
+  if (!is.null(beg)) {
+    spikes <- lapply(spikes, jay.filter.for.min, min=beg)
+  } else {
+    beg <- spikes.range[1]
+  }
+
+  ## Remove any channels that have zero spikes (which can happen if
+  ## the beg, end range is too narrow, or if a datafile is empty,
+  ## which happens sometime for the Feller data.
+  spikes <- remove.empty.channels(spikes)
+
+
+  if (!is.null(ids)) {
+    ## Filter out some channel names, either inclusively or exclusively.
+    spikes <- filter.channel.names(spikes, ids)
+  }
+  
+  rec.time <- c(beg, end)
+
+  channels <- names(spikes)
+
+  
+  ## Count the number of spikes per channel, and label them.
+  nspikes <- sapply(spikes, length)
+
+  ## meanfiring rate is the number of spikes divided by the (time of
+  ## last spike - time of first spike).  
+  meanfiringrate <- nspikes/ ( end - beg)
+
+  rates <- make.spikes.to.frate(spikes, time.interval=time.interval,
+                                beg=beg, end=end)
+
+  ## Parse the channel names to get the cell positions.
+  layout <- make.iit.layout(channels)
+
+  ## TODO; worry about multiple units recorded at the same location?
+  unit.offsets <- NULL                  #default value.
+  
+  ## check that the spikes are monotonic.
+  check.spikes.monotonic(spikes)
+
+
+  rates <- make.spikes.to.frate(spikes, time.interval=time.interval,
+                                beg=beg, end=end)
+
+  
+  res <- list( channels=channels,
+              spikes=spikes, nspikes=nspikes, NCells=length(spikes),
+              meanfiringrate=meanfiringrate,
+              file=filename,
+              layout=layout,
+              rates=rates,
+              unit.offsets=unit.offsets,
+              rec.time=rec.time
+              )
+  class(res) <- "mm.s"
+
+  max.dist <- sqrt(2*(64*40)^2)
+  breaks = seq(from=0, to=max.dist, by=50)
+  res$corr = corr.index(res, breaks)
+
+  res
+
+}
+
+
+make.iit.layout <- function(positions) {
+  ## make the layout for SANGER MEA (cf. make.sanger1.layout)
+  ## This is a hexagonal grid.
+  spacing <- 40;  #20um diam electrodes
+  xlim <- ylim <- c(0, 64*spacing)
+
+
+
+  ## parse the channel names into rows and columns.
+  regexp <- "Ch([0-9]+)\\.([0-9]+)"
+  r <- as.integer(gsub(regexp, "\\1", positions))
+  c <- as.integer(gsub(regexp, "\\2", positions))
+
+  rows = (r-1)*spacing
+  cols = (c-1)*spacing
+  pos <- cbind(rows, cols)
+  
+  rownames(pos) <- positions
+  
+  layout <- list(xlim=xlim, ylim=ylim, spacing=spacing,
+                 pos=pos)
+
+  class(layout) <- "mealayout.hi"
+
+  layout
+
+}
+
+
+plot.mealayout.hi <- function(x, use.rownames=FALSE, ...) {
+  ## Plot the MEA layout, high density version
+  pos <- x$pos
+  plot(pos, asp=1,
+       xlim=x$xlim, ylim=x$ylim,
+       bty="n",
+       xlab="", ylab="", pch=20)
+}
