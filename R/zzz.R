9c773101d99cb72a9e614e4fe105a22c507120a5
Stephen Eglen <sje30@cam.ac.uk>
2012-02-25 08:02:54 +0000

    Remove .First.lib and use the NAMESPACE file to load the dynamic library.

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,6 @@
 ## Author: Stephen J Eglen
 ## Copyright: GPL
 
-.First.lib <- function(lib, pkg) {
-  library.dynam("sjemea", pkg, lib)
-}
-
 .Last.lib <- function (libpath) {
   ## Run when the package is being unloaded.  This allows us to easily test
   ## packages within same session when dynlib is updates.
