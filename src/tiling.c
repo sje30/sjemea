233f638667756428f9bfd2c154a7734f70ea3ec0
Stephen J. Eglen <sje30@cam.ac.uk>
2014-05-12 09:50:22 +0100

    New version from Catherine, with start and end time of recording passed.

--- src/tiling.c
+++ src/tiling.c
@@ -12,6 +12,8 @@ double run_P(int N1,int N2,double dt,double *spike_times_1,double *spike_times_2
 	j=0;
 	for(i=0;i<=(N1-1);i++){
 		while(j<N2){	
+//check every spike in train 1 to see if there's a spike in train 2 within dt  (don't count spike pairs)
+// don't need to search all j each iteration
 			if(fabs(spike_times_1[i]-spike_times_2[j])<=dt){
 				Nab=Nab+1;	
 				break;				
@@ -29,107 +31,70 @@ double run_P(int N1,int N2,double dt,double *spike_times_1,double *spike_times_2
 
 
 
-double run_T(int N1v,double dtv,double Tv,double *spike_times_1,double *spike_times_2){
+double run_T(int N1v,double dtv,double startv, double endv, double *spike_times_1){
 	
-	int i;
-	int j;
-	int u;
 	double dt= dtv;
-	double start;
-	double end;
+	double start=startv;
+	double end=endv;
 	int N1= N1v;
-	double T= Tv;
 	double time_A;
+	int i=0;
+	double diff;
 	
+//maximum 
+	time_A=2*(double)N1*dt;
 
-	if(N1>1){
-	if((spike_times_1[1]-spike_times_1[0])> 2*dt){
-		if(spike_times_1[0]>dt){
-			time_A=2*dt;				
-		}
-		else{					
-			time_A=spike_times_1[0]+dt;
-		}
-		u=1;
-	}
-	else{
-		start=spike_times_1[0];
-		end=spike_times_1[1];
-		for(j=0;j<(N1-1);j++){
-			if((spike_times_1[j+1]-spike_times_1[j])<=2*dt){
-				end=spike_times_1[j+1];
-			}
-			else{
-				break;
-			}		
-		}
-		if(spike_times_1[0]<=dt){
-			time_A=end+dt;
-		}
-		else{
-			time_A=end-start+2*dt;
-		}
-		u=j+1;
-	}
-
-	i=u;
-
-//n.b. if second spike is within dt of first spike, it'll be picked up by the first loop
-	
-	while(i <=(N1-1)){
-		if(i==(N1-1)){
-			if((T-spike_times_1[i])<=dt){				
-				time_A=time_A+dt+T-spike_times_1[i];
-			}
-			else{
-				time_A=time_A+2*dt;				
-			}
-			i++;
-		}else{
-			if((spike_times_1[i+1]-spike_times_1[i])<=2*dt){
-				start=spike_times_1[i];
-				end=spike_times_1[i+1];
-				
-				for(j=i;j<(N1-1);j++){
-					
-					if((spike_times_1[j+1]-spike_times_1[j])<=2*dt){
-						end=spike_times_1[j+1];	
-					}
-						else{
-							break;
-					}
-					
-				}
-				if((end+dt)>T){
-					time_A=time_A+(T-start)+dt;					
-				}
-				else{
-					time_A=time_A+(end-start)+2*dt;						
-				}
-				i=j+1;	
-			}
-			else{
-				time_A=time_A+2*dt;
-				i=i+1;
-			}
-		}
-	}
-	}
+// if just one spike in train 
 	if(N1==1){
-	  if(spike_times_1[0]<dt){
-	    time_A=spike_times_1[0]+dt;
+		
+	  if((spike_times_1[0]-start)<dt){
+	    	time_A=time_A-start+spike_times_1[0]-dt;
 	  }
-	  else if((spike_times_1[0]+dt)>T){
-	    time_A=dt+T-spike_times_1[0];
+	  else if((spike_times_1[0]+dt)>end){
+	   	 time_A=time_A-spike_times_1[0]-dt+end;
 	      }
-	  else{    
-	  time_A=2*dt;
-	  }
+	  
 	}
-	return time_A;
-}
+	
+//if more than one spike in train
+	else{
+			
 
-void run_TM(int *N1v,int *N2v,double *dtv,double *Tv,double *index,double *spike_times_1,double *spike_times_2){
+			while(i<(N1-1)){
+			
+				diff=spike_times_1[i+1]-spike_times_1[i];
+				
+				if(diff<2*dt){
+					//subtract overlap 	
+					time_A=time_A-2*dt+diff;
+
+				}
+				
+				i++;
+			}
+				
+			//check if spikes are within dt of the start and/or end, if so just need to subract
+			//overlap of first and/or last spike as all within-train overlaps have been accounted for
+			//(in the case that more than one spike is within dt of the start/end
+			
+			if((spike_times_1[0]-start)<dt){
+				
+			  time_A=time_A-start+spike_times_1[0]-dt;
+			}
+
+
+			if((end-spike_times_1[N1-1])<dt){
+				
+			  time_A=time_A-spike_times_1[N1-1]-dt+end;
+			}
+              	}
+	
+	return time_A;	
+}
+	
+
+
+void run_TM(int *N1v,int *N2v,double *dtv,double *Time,double *index,double *spike_times_1,double *spike_times_2){
 
 	double TA;
 	double TB;
@@ -138,15 +103,18 @@ void run_TM(int *N1v,int *N2v,double *dtv,double *Tv,double *index,double *spike
 	int N1= *N1v;
 	int N2= *N2v;
 	double dt= *dtv;
-	double T=*Tv;
+	double T;
+
+	
 	
 	if(N1==0 || N2==0){
-	*index=NAN;
+	*index=R_NaN;
 	}
 	else{
-	TA=run_T(N1,dt,T, spike_times_1, spike_times_2);
+	T=Time[1]-Time[0];
+	TA=run_T(N1,dt,Time[0],Time[1], spike_times_1);
 	TA=TA/T;
-	TB=run_T(N2,dt,T, spike_times_2, spike_times_1);
+	TB=run_T(N2,dt,Time[0],Time[1], spike_times_2);
 	TB=TB/T;
 	PA=run_P(N1,N2,dt, spike_times_1, spike_times_2);
 	PA=PA/(double)N1;
